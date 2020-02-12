787a60bc02dde31eef6748893e9bf121605a33a6
arth0056 <arth0056@stud.kea.dk>
2020-02-12 17:46:42 +0100

    pop_up_babushka_done

--- 07.02.01_bistro_babushka/babushka.js
+++ 07.02.01_bistro_babushka/babushka.js
@@ -2,6 +2,7 @@ let personer = [];
 const googleData = "https://spreadsheets.google.com/feeds/list/17Dd7DvkPaFamNUdUKlrFgnH6POvBJXac7qyiS6zNRw0/od6/public/values?alt=json";
 let filter = "alle";
 document.addEventListener("DOMContentLoaded", startUp);
+const detalje = document.querySelector("#pop_up");
 
 
 
@@ -62,12 +63,27 @@ function visTing() {
             console.log("hej");
             let klon = dyrTemplate.cloneNode(true).content;
             klon.querySelector("h3").textContent = ting.gsx$navn.$t;
-            //            klon.querySelector("img").src = ting.gsx$billede.$t;
+            klon.querySelector("img").src = "pics/small/" + ting.gsx$billede.$t + "-sm.jpg";
             klon.querySelector(".p1").textContent += ting.gsx$kategori.$t;
             klon.querySelector(".p2").textContent += ting.gsx$kort.$t;
             klon.querySelector(".p3").textContent += ting.gsx$pris.$t + " kr";
+            klon.querySelector(".tik").addEventListener("click", function () {
+                visPopUp(ting);
+            });
             container.appendChild(klon);
         }
     });
 
+}
+
+function visPopUp(ting) {
+    detalje.classList.remove("hide");
+    detalje.querySelector("#pop_up_button").addEventListener("click", () => detalje.classList.add("hide"));
+    console.log(ting.gsx$navn.$t);
+    detalje.querySelector(".pop_up_h1").textContent = ting.gsx$navn.$t;
+    detalje.querySelector("p").textContent = ting.gsx$kategori.$t;
+    detalje.querySelector("p + p").textContent = ting.gsx$lang.$t;
+    detalje.querySelector("p + p + p").textContent = ting.gsx$pris.$t + " kr.";
+
+    detalje.querySelector("img").src = "pics/large/" + ting.gsx$billede.$t + ".jpg";
 }
